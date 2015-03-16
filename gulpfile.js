c73db6b0a647b86acc86c1ffd473dcf5f5ebe230
Michael Littman <mlittman@buzzpoints.com>
2015-03-16 17:25:08 -0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp            = require('gulp'),
+    server          = require('gulp-webserver')
+    ;
+
+gulp.task('serve', function(){
+    return gulp.src('src')
+        .pipe(server({
+          directoryListing: true,
+          open: true
+      }));
+});
